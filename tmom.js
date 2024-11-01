c5257ad338651668f06ae54e9e7f048756bdb8ec
Kerehamaa <164129171+Kerehamaa@users.noreply.github.com>
2024-11-02 09:44:50 +1300

    Inital Upload

---
+++ tmom.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const pages = ["home", "more", "info"];
+    
+    pages.forEach(page => {
+        const element = document.getElementById(page);
+        if (element) {
+            element.addEventListener("click", function () {
+                window.location.href = page + '.html';
+            });
+        } else {
+            console.warn(`Element with ID '${page}' not found.`);
+        }
+    });
+});
